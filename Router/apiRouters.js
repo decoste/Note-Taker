6e78e8be78cdfb589c41a61370487f9930160b03
ala decoste <alshareefialaa@gmail.com>
2020-12-13 00:41:13 -0500

    Delete function is working!

--- Router/apiRouters.js
+++ Router/apiRouters.js
@@ -1,5 +1,4 @@
 //Dependencies
-
 const fs = require('fs');
 const path = require('path');
 const notes = require("../db/db.json");
@@ -7,31 +6,30 @@ const notes = require("../db/db.json");
 //questions/23327010/how-to-generate-unique-id-with-node-js
 const { v4: uuidv4 } = require('uuid');
 
-module.exports = function(app) {
-
-    app.get("/api/notes", function(req, res) {
-       
+module.exports = function (app) {
+    //get
+    app.get("/api/notes", function (req, res) {
         res.json(notes);
     });
-
+    //post
     let newId = uuidv4();
-    app.post("/api/notes", function(req, res) {
+    app.post("/api/notes", function (req, res) {
         const newNote = {
             title: req.body.title,
             text: req.body.text,
             id: newId
-          };
-
-  notes.push(newNote);
-
-  res.json(newNote);
-    
+        };
+        notes.push(newNote);
+        res.json(notes);
     });
-  
- 
-    app.delete("/api/notes", function(req, res) {
-  
+    //delete
+    app.delete("/api/notes/:id", function (req, res) {
+
+        const index = notes.findIndex((notes) =>
+        notes.id === req.params.id);
+        notes.splice(index, 1);
+        res.json(notes);
     });
-  
-  };
-  
+
+};
+
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3002;
+const PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
