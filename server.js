1bc2b028ba81fecf5af048acce98dea1910fb160
ala decoste <alshareefialaa@gmail.com>
2020-12-12 20:15:45 -0500

    Installed express and Sat up the Express app to handle data parsing.

---
+++ server.js
@@ -0,0 +1,15 @@
+// Dependencies
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+// Starts our server
+app.listen(PORT, function() {
+    console.log("Server is listening on PORT: " + PORT);
+  });
